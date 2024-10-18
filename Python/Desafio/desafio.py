c08e280cbb8387a63c3844ad80c292774a13f807
luiza <luizalkarlec@gmail.com>
2024-10-17 21:08:12 -0300

    Create desafio.py

---
+++ Python/Desafio/desafio.py
@@ -0,0 +1,12 @@
+# programa que recebe um arquivo texto, palavra de substituicao, palavra de pesquisa
+
+# solocitar ao usuario um arquivo texto
+# solicitar a palavra ou expressão a ser substituida (origem)
+# solocitar a palavra ou expressão que irá substituir (destino)
+
+# associar e abrir o arquivo a um leitor de arquivo
+# transferir o conteudo do arquivo para dentro de uma variavel string
+# localizar a palavra origem e substituir pela palavra destino na variavel string
+
+# associar e abrir o arquivo a um escritor de arquivo
+# gravar a variavel do tipo string como texto alterado no arquivo
