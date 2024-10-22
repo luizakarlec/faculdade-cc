08b8231249a1159e85ee07432333154fbbe1beb3
luiza <luizalkarlec@gmail.com>
2024-10-22 08:16:18 -0300

    Create 11. maior elemento vetor.c

---
+++ Algoritmos B/Ponteiros/Lista 6/11. maior elemento vetor.c
@@ -0,0 +1,4 @@
+/*
+Ler um vetor de inteiros com 20 elementos. Desenvolver uma função que encontra o maior elemento do vetor e
+retorne o ponteiro para o maior elemento. A função main deve exibir o maior elemento do vetor.
+*/
