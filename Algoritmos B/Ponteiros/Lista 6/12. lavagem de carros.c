3510a7507cb7cf3ff3f044747a69c8ef959a3cc9
luiza <luizalkarlec@gmail.com>
2024-10-22 08:17:23 -0300

    Create 12. lavagem de carros.c

---
+++ Algoritmos B/Ponteiros/Lista 6/12. lavagem de carros.c
@@ -0,0 +1,10 @@
+/*
+ Considere que em uma lavagem de carros, é necessário implementar um programa que controle a entrada e
+saída de veículos. Ao chegar um veículo, são registrados a placa, o nome e telefone do proprietário, valor a ser
+pago e a situação (pago ou não pago). Considere que são atendidos no máximo 10 veículos.
+Desenvolva este programa usando funções para:
+– obter os dados de um veículo
+– registrar o pagamento da lavagem de um veículo
+– mostrar todos os dados de um veículo
+– calcular o valor total recebido.
+*/
