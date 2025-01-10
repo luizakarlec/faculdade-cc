3f6e7ba58c88b2003328a03e4b9080d7b9c35705
luiza <luizalkarlec@gmail.com>
2025-01-09 22:55:23 -0300

    Create 06. potencia.c

---
+++ Algoritmos A/Estrutura de Desvio/Lista 2/06. potencia.c
@@ -0,0 +1,35 @@
+/*
+Existem números de 4 dígitos (entre 1000 e 9999) que obedecem à seguinte característica: se
+dividirmos o número em dois números de dois dígitos, um composto pela dezena e pela
+unidade, e outro pelo milhar e pela centena, se somarmos estes dois novos números gerando
+um terceiro, o quadrado deste terceiro número é exatamente o número original de quatro
+dígitos. Por exemplo:
+2025 -> dividindo: 20 e 25 -> somando 20 e 25 tem-se 45  452= 2025.
+Escreva um programa para ler um número e verificar se ele obedece a esta característica.
+*/
+
+#include<stdio.h>
+int main(){
+    int x,dezenaunidade,milharcentena;
+    int soma,quadrado;
+
+    printf("escreva um numero de 4 digitos: ");
+    scanf("%i",&x);
+
+    dezenaunidade=x%100;
+    milharcentena=x/100;
+
+    soma=dezenaunidade+milharcentena;
+    quadrado=soma*soma;
+
+    printf("%i + %i = %i\n",milharcentena,dezenaunidade,soma);
+    printf("%i ao quadrado = %i\n",soma,quadrado);
+
+    if(quadrado==x){
+        printf("obedeceu a caracteristica\n");
+    }else{
+        printf("nao obedeceu a caracteristica\n");
+    }
+
+    return 0;
+}
