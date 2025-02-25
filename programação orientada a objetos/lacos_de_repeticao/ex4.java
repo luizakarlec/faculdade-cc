1b1b364046bb0d6f45cb9b5e6547c2360736cbaf
luiza <luizalkarlec@gmail.com>
2025-02-25 09:58:05 -0300

    Create ex4.java

---
+++ programação orientada a objetos/lacos_de_repeticao/ex4.java
@@ -0,0 +1,13 @@
+package ex4;
+
+public class Ex4 {
+	
+	public static void main(String args[]) {
+		
+		int soma = 0;
+		for(int i=0; i<=100; i++) {
+			soma += i;
+		}
+		System.out.println(soma);
+	}
+}
