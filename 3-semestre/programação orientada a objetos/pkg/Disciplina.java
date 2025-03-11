63d52b0c98d1f2eedd08924d7e469c7360585a57
luiza <luizalkarlec@gmail.com>
2025-03-11 10:02:15 -0300

    Update Disciplina.java

--- 3-semestre/programação orientada a objetos/pkg/Disciplina.java
+++ 3-semestre/programação orientada a objetos/pkg/Disciplina.java
@@ -1,4 +1,5 @@
 package pkg;
+import java.util.Scanner;
 
 public class Disciplina {
 
@@ -6,12 +7,26 @@ public class Disciplina {
 		Laboratorio lab = new Laboratorio();
 		Professor p = new Professor();
 		
-		lab.local = "311";
+		lab.local = "Laboratorio 311/P4";
 		p.nome = "Ricardo";
 		
 		System.out.println("O nome do professor é: "+p.nome);
 		System.out.println("A local da aula é: "+lab.local);
-
+		
+		Professor p2 = new Professor();
+		Laboratorio lab2 = new Laboratorio();
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Nome professor = ");
+		p2.nome = teclado.nextLine();
+		System.out.print("Nome local = ");
+		lab2.local = teclado.nextLine();
+		
+		System.out.println("O nome do professor é: "+p2.nome);
+		System.out.println("A local da aula é: "+lab2.local);
+		
+		teclado.close();
 	}
 
 }
