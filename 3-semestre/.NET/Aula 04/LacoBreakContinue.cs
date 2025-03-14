37ffcb0fc89d931d9f43d7b214617b4fa4f8e8c3
luiza <luizalkarlec@gmail.com>
2025-03-14 12:42:20 -0300

    Create LacoBreakContinue.cs

---
+++ 3-semestre/.NET/Aula 04/LacoBreakContinue.cs
@@ -0,0 +1,29 @@
+namespace Aula5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            
+            for (i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            for (i = 10; i > 0; i--)
+            {
+                if(i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
