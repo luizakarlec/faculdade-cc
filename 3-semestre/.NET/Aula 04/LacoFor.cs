4479f37e857e70357ea114e0163b69465580c18b
luiza <luizalkarlec@gmail.com>
2025-03-14 12:23:40 -0300

    Create LacoFor.cs

---
+++ 3-semestre/.NET/Aula 04/LacoFor.cs
@@ -0,0 +1,18 @@
+namespace Aula5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
