fcacbb641427ea38cfe56a0dc5d68a34752ecee4
luiza <luizalkarlec@gmail.com>
2025-03-21 14:05:03 -0300

    Create JogoClasse.cs

---
+++ 3-semestre/.NET/Aula 5/JogoClasse.cs
@@ -0,0 +1,10 @@
+namespace Exercicio01
+{
+    internal class Jogo
+    {
+        public String titulo;
+        public String genero;
+        public String plataforma;
+        public int anoLancamento;
+    }
+}
