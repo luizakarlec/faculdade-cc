c043be1e0d6522df1cfcfb38ef0c707e220a351c
luiza <luizalkarlec@gmail.com>
2025-03-28 12:33:30 -0300

    Update Lampada.cs

--- 3-semestre/.NET/Aula 06/ExemploLampada/Lampada.cs
+++ 3-semestre/.NET/Aula 06/ExemploLampada/Lampada.cs
@@ -1,1 +1,45 @@
-Console.WriteLine("Data de nascimento = "+dataNascimento);
+namespace ExemploLampada
+{
+    internal class Lampada
+    {
+        public bool ligada;
+        public double potencia;
+        public string cor;
+
+        public Lampada(double p, string c)
+        {
+            potencia = p;
+            cor = c;
+        }
+
+        public void ligar()
+        {
+            if(!ligada)
+            {
+                ligada = true;
+                Console.WriteLine("Ligando lâmpada!");
+            }
+            else
+            {
+                Console.WriteLine("Esta ligada!");
+            }
+        }
+       
+        public void desligar()
+        {
+            if(ligada)
+            {
+                ligada = false;
+                Console.WriteLine("Desligando lâmpada!");
+            }
+            else
+            {
+                Console.WriteLine("Esta desligada!");
+            }
+        }
+        public bool estaLigada()
+        {
+            return ligada;
+        }
+    }
+}
