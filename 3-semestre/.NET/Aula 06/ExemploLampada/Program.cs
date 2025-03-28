d9ddf55a052fc7e8da502b70756d940151bab231
luiza <luizalkarlec@gmail.com>
2025-03-28 12:33:56 -0300

    Create Program.cs

---
+++ 3-semestre/.NET/Aula 06/ExemploLampada/Program.cs
@@ -0,0 +1,24 @@
+namespace ExemploLampada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada l1 = new Lampada(120, "rosa");
+            Console.WriteLine("Potência: "+l1.potencia);
+            Console.WriteLine("Cor: "+l1.cor);
+
+            l1.desligar();
+            l1.ligar();
+            l1.ligar();
+            l1.ligar();
+            l1.ligar();
+            l1.ligar();
+            l1.desligar();
+            l1.desligar();
+
+            bool retorno = l1.estaLigada();
+            Console.WriteLine("Retorno do método: "+retorno );
+        }
+    }
+}
