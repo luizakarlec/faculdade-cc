f20e4ea2c3b78a07ffb447c3d15394c5c2c7d20f
luiza <luizalkarlec@gmail.com>
2025-04-12 15:40:53 -0300

    Create Veiculo.cs

---
+++ 3-semestre/.NET/Aula 07/Exercicio02/Veiculo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    abstract class Veiculo
+    {
+        public String marca;
+        public String modelo;
+
+        public abstract void Dirigir();
+    }
+}
