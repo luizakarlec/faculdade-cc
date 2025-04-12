641b3db997afd0bf4341e34db4fffe07eca0f615
luiza <luizalkarlec@gmail.com>
2025-04-12 15:44:14 -0300

    Create ContaPoupanca.cs

---
+++ 3-semestre/.NET/Aula 07/Exercicio03/ContaPoupanca.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    internal class ContaPoupanca : ContaBancaria
+    {
+        public double deposito;
+        public double saque;
+
+        public override void Depositar()
+        {
+            saldo = saldo + deposito;
+            Console.WriteLine("Depositado!");
+        }
+        public override void Sacar()
+        {
+            if (saque <= saldo)
+            {
+                saldo = saldo - saque;
+                Console.WriteLine("Sacado!");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente.");
+            }
+        }
+    }
+}
