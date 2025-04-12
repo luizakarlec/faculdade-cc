73cfcd4f68fa2dbd9694c68e0158378af06b03fb
luiza <luizalkarlec@gmail.com>
2025-04-12 15:39:03 -0300

    Create Pessoa.cs

---
+++ 3-semestre/.NET/Aula 07/Exercicio01/Pessoa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    abstract class Pessoa
+    {
+        public String nome;
+        public int idade;
+
+        public abstract void apresentar();
+    }
+}
