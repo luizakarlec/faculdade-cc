b4c6925e970a4cda00783a379573f274a4b61042
luiza <luizalkarlec@gmail.com>
2025-04-15 10:36:32 -0300

    Create Desenho.java

---
+++ 3-semestre/programação orientada a objetos/heranca/heranca_multipla/classes/Desenho.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Desenho {
+	protected String nomeAutor;
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+	
+}
