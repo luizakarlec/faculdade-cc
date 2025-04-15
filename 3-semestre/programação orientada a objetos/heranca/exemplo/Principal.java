4fa8f08e49c1cd23a6dcc3ab05d84c781fb69214
luiza <luizalkarlec@gmail.com>
2025-04-15 09:14:56 -0300

    Create Principal.java

---
+++ 3-semestre/programação orientada a objetos/heranca/exemplo/Principal.java
@@ -0,0 +1,19 @@
+package pacote;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Carro c = new Carro();
+		c.setNome("Fusca");
+		c.exibeMsg();
+		
+		Onibus o = new Onibus();
+		o.setNome("Marcopolo");
+		o.setModdelo("OF-1519");
+		
+		o.exibeMsg();
+		
+		System.out.println("O nome do carro: "+o.getNome());
+		System.out.println("Modelo do onibus: "+o.getModelo());
+	}
+}
