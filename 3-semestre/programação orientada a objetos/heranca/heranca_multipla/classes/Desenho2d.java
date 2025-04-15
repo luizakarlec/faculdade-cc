e19f82c1d8dc197bcba0c6dae6e3e8fbe98e40ca
luiza <luizalkarlec@gmail.com>
2025-04-15 10:36:50 -0300

    Create Desenho2d.java

---
+++ 3-semestre/programação orientada a objetos/heranca/heranca_multipla/classes/Desenho2d.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Desenho2d extends Desenho{
+	protected int largura;
+	protected int altura;
+	
+	public Desenho2d(int largura, int altura) {
+		super();
+		this.largura = largura;
+		this.altura = altura;
+	}
+}
