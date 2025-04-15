db17bcf69be5aa45fef832bccf36dbf9178f1229
luiza <luizalkarlec@gmail.com>
2025-04-15 09:15:15 -0300

    Create Onibus.java

---
+++ 3-semestre/programação orientada a objetos/heranca/exemplo/Onibus.java
@@ -0,0 +1,14 @@
+package pacote;
+
+public class Onibus extends Carro{
+	//classe Onibus extende Carro
+	protected String modelo;
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModdelo(String modelo) {
+		this.modelo = modelo;
+	}
+}
