46d0d685a9daa4337d3b120bce139c066e9c876c
luiza <luiza.karlec@ufn.edu.br>
2025-04-29 10:33:53 -0300

    Add files via upload

---
+++ 3-semestre/programação orientada a objetos/polimorfismo/jogo_rapido2/PessoaOi.java
@@ -0,0 +1,11 @@
+package jogo_rapido2;
+
+public class PessoaOi {
+	public void dizerOla() {
+		System.out.println("Olá!");
+	}
+	
+	public void dizerOla(String nome) {
+		System.out.println("Olá, "+nome+"!");
+	}
+}
---
+++ 3-semestre/programação orientada a objetos/polimorfismo/jogo_rapido2/Principal.java
@@ -0,0 +1,10 @@
+package jogo_rapido2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		PessoaOi p = new PessoaOi();
+		p.dizerOla();
+		p.dizerOla("luiza");
+	}
+}
