66ec0ccda0c4b71ee4f3f3d3fb0db2e90277bf73
luiza <luizalkarlec@gmail.com>
2025-05-23 10:50:37 -0300

    Create lista.h

---
+++ 3-semestre/estrutura de dados/Aula 9/lista.h
@@ -0,0 +1,37 @@
+typedef struct no {
+    int dado;
+    struct no *prox;
+} Lista;
+ 
+Lista *inserir(int dado, Lista *lista) {
+    if (lista && dado >= lista->dado) {
+        if (dado != lista->dado) {
+            //avançar na lista
+            lista->prox = inserir(dado, lista->prox);
+        }
+        return lista;
+    } else { //cheguei no final da lista
+        Lista *novo = (Lista*)malloc(sizeof(Lista));
+        novo->dado = dado;
+        if (lista) {
+            novo->prox = lista;
+        } else {
+            novo->prox = NULL;
+        }
+        return novo;
+    }
+}
+ 
+void exibir(Lista *lista) {
+    if (lista) {
+        cout << lista->dado << endl;
+        exibir(lista->prox);
+    }
+}
+ 
+int contar(Lista *lista) {
+    if (lista) {
+        return 1 + contar(lista->prox);
+    }
+    return 0;
+}
