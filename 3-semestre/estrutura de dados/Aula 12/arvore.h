c556d7b2a8e2d8ba6538ca2ca1f69418c6430608
luiza <luizalkarlec@gmail.com>
2025-06-13 10:48:52 -0300

    Update arvore.h

--- 3-semestre/estrutura de dados/Aula 12/arvore.h
+++ 3-semestre/estrutura de dados/Aula 12/arvore.h
@@ -88,3 +88,89 @@ int localizarNivel(int dado, Arvore *raiz) {
     }
     return -1; //cheguei no vazio e nao encontrei o valor
 }
+ 
+void exibirFolhas(Arvore *raiz) {
+    if (raiz) {
+        if (!raiz->esq && !raiz->dir) {
+            cout << raiz->dado << "\t";
+        } else {
+            exibirFolhas(raiz->esq);
+            exibirFolhas(raiz->dir);
+        }
+    }
+}
+ 
+int contarFolhas(Arvore *raiz) {
+    if (raiz) {
+        if (!raiz->esq && !raiz->dir) {
+            return 1;
+        }
+        return 0 + contarFolhas(raiz->esq) + contarFolhas(raiz->dir);
+    }
+    return 0;
+}
+ 
+Arvore *podar(Arvore *raiz) {
+    if (raiz) {
+        if (!raiz->esq && !raiz->dir) {
+            //apagar o no folha
+            free(raiz);
+            return NULL;
+        }
+        raiz->esq = podar(raiz->esq);
+        raiz->dir = podar(raiz->dir);
+        return raiz;
+    }
+    return NULL;
+}
+ 
+Arvore *limpar(Arvore *raiz) {
+    if (raiz) {
+        raiz->esq = limpar(raiz->esq);
+        raiz->dir = limpar(raiz->dir);
+        free(raiz);
+    }
+    return NULL;
+}
+ 
+Arvore *remover(int dado, Arvore *raiz) {
+    if (raiz) {
+        if (dado == raiz->dado) {//localizei o dado e remover
+            //verificar se eh folha
+            if (!raiz->esq && !raiz->dir) {
+                free(raiz);
+                return NULL;
+            }
+            //verificar se ha um filho somente
+            if ((raiz->esq && !raiz->dir) || (raiz->dir && !raiz->esq)) {
+                Arvore *tmp;
+                if (raiz->esq) {
+                    tmp = raiz->esq;    
+                }
+                if (raiz->dir) {
+                    tmp = raiz->dir;    
+                }
+                free(raiz);
+                return tmp;
+            }
+            //se estou neste ponto eh porque o nodo tem dois filhos e o filho da esquerda serah o novo pai
+            Arvore *novoPai = raiz->esq;
+            Arvore *tmp = novoPai->dir;
+            novoPai->dir = raiz->dir;
+            free(raiz);
+ 
+            Arvore *p;
+            for (p = novoPai->dir; p->esq; p = p->esq);
+           
+            p->esq = tmp;
+            return novoPai;
+        }
+        if (dado < raiz->dado) {
+            raiz->esq = remover(dado, raiz->esq);
+        } else {
+            raiz->dir = remover(dado, raiz->dir);
+        }
+        return raiz;
+    }
+    return NULL;
+}
