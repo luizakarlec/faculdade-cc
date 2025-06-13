9bff1ce46bb1e8b251d81d5432c756ed16b5e3b4
luiza <luizalkarlec@gmail.com>
2025-06-13 10:10:04 -0300

    Create principal.cpp

---
+++ 3-semestre/estrutura de dados/Aula 12/principal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+ 
+#include "arvore.h"
+ 
+int main() {
+    Arvore *arvore = NULL;
+    int qtd = 10;
+    int numero;
+   
+    srand(time(NULL));
+ 
+    for (;qtd > 0; qtd--) {
+        numero = rand() % 10;
+        cout << "Numero gerado: " << numero << endl;
+        arvore = inserir(numero, arvore);
+    }
+ 
+    cout << endl;
+    cout << "Total de nos: " << contarNodos(arvore) << endl;
+    exibirArvore(0, arvore);
+ 
+    cout << "Digite numero para pesquisa/delecao: ";
+    cin >> numero;
+ 
+    cout << "Localizado? " << localizarNivel(numero, arvore) << endl;
+ 
+    return 1;
+}
