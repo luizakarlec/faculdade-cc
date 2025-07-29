9b2548e9f7b80cda999f9ccd5b511ee3dfdfd56e
luiza <luiza.karlec@ufn.edu.br>
2025-07-29 10:50:27 -0300

    Add files via upload

---
+++ 4-semestre/laboratorio de desenvolvimento de software/revisao_poo/pacote/Onibus.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Onibus extends Carro {
+    protected String modelo;
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
---
+++ 4-semestre/laboratorio de desenvolvimento de software/revisao_poo/pacote3/Triangulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote3;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Triangulo extends FormaGeometrica {
+    @Override
+    public double calcularArea(double base, double altura){
+        return (base * altura)/2;
+    }
+}
