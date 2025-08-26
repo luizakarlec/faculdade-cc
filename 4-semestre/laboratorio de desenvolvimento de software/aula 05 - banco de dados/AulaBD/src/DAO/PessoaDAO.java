5779e35b7d5d2b49b1e2ff16dd840e3438405c85
luiza <luiza.karlec@ufn.edu.br>
2025-08-26 10:51:25 -0300

    Add files via upload

---
+++ 4-semestre/laboratorio de desenvolvimento de software/aula 05 - banco de dados/AulaBD/src/DAO/PessoaDAO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import beans.Pessoa;
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.*;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class PessoaDAO {
+    
+    private Conexao conexao;
+    private Connection conn;
+    
+    public PessoaDAO(){
+        this.conexao = new Conexao();
+        this.conn = this.conexao.getConexao();
+    }
+    
+    public void inserir(Pessoa pessoa){
+        try {
+            String sql = "INSERT INTO pessoa (nome, sexo, idioma)" 
+                    + "VALUES (?, ?, ?);";
+            
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, pessoa.getNome());
+            stmt.setString(2, pessoa.getSexo());
+            stmt.setString(3, pessoa.getIdioma());
+            stmt.execute();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao inserir: "+ex.getMessage());
+        }
+    }
+}
