08f3a5731de9f2c55f48a390dca1830716252360
luiza <luizalkarlec@gmail.com>
2025-09-30 09:39:43 -0300

    Update comandos.sql

--- 4-semestre/laboratorio de desenvolvimento de software/aula 09/comandos.sql
+++ 4-semestre/laboratorio de desenvolvimento de software/aula 09/comandos.sql
@@ -16,18 +16,17 @@ INSERT INTO pessoa(nome, sexo, idioma)
 VALUES ("Luiza", "F", "Português");
 
 INSERT INTO pessoa (nome, sexo, idioma)
-    ->     VALUES
-    ->     ("Gerald", "M", "Inglês"),
-    ->     ("William", "M", "Inglês"),
-    ->     ("Umberto", "M", "Espanhol"),
-    ->     ("Jostein", "M", "Alemão"),
-    ->     ("Stephen", "M", "Holandês");
-
+VALUES
+("Gerald", "M", "Inglês"),
+ ("William", "M", "Inglês"),
+ ("Umberto", "M", "Espanhol"),
+ ("Jostein", "M", "Alemão"),
+ ("Stephen", "M", "Holandês");
 
 mysql> CREATE TABLE veiculo(
-    -> id int AUTO_INCREMENT PRIMARY KEY,
-    -> modelo varchar(30),
-    -> placa varchar(7),
-    -> id_pessoa int NOT NULL,
-    -> foreign key(id_pessoa) references pessoa(id)
-    -> );
+id int AUTO_INCREMENT PRIMARY KEY,
+modelo varchar(30),
+placa varchar(7),
+id_pessoa int NOT NULL,
+foreign key(id_pessoa) references pessoa(id)
+);
