b30d0a1e1af590787538130e12e466d1a3817411
luiza <luizalkarlec@gmail.com>
2025-10-06 14:56:53 -0300

    Update comandos.sql

--- 4-semestre/laboratorio de desenvolvimento de software/aula 10/comandos.sql
+++ 4-semestre/laboratorio de desenvolvimento de software/aula 10/comandos.sql
@@ -1,7 +1,26 @@
+-- Escola
 CREATE TABLE disciplina (
 id int AUTO_INCREMENT PRIMARY KEY,
 nome varchar(50) NOT NULL,
 carga_horaria int NOT NULL,
 id_professor INT,
 FOREIGN KEY (id_professor) REFERENCES professores(id)
+);
+
+-- Produto
+create database produto;
+use produto;
+
+create table categorias(
+id int auto_increment primary key,
+nome varchar(50)
+);
+
+create table produtos(
+id int auto_increment primary key,
+nome varchar(50),
+preço decimal(10,2),
+quantidade int,
+id_categoria int,
+foreign key (id_categoria) references categorias(id)
 );
